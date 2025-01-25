73afad09c1018e554aba2f3078fb6e871a8a8089
Chr1ztt <chilllikehuman@gmail.com>
2025-01-25 07:51:39 +0700

    initial commit

---
+++ app/src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from "express"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send({ message: "GET pong" })
+})
+
+app.post("/ping", (req: Request, res: Response) => {
+  res.send({ message: "POST pong" })
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
+
