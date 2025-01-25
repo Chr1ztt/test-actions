8945b28a0701e66f6360d227f2ae792795b36817
Chr1ztt <chilllikehuman@gmail.com>
2025-01-25 15:10:06 +0700

    refactor: change port to use variable

--- app/src/index.ts
+++ app/src/index.ts
@@ -12,8 +12,8 @@ app.post("/ping", (req: Request, res: Response) => {
   res.send({ message: "POST pong" })
 })
 
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
 
